a18e17e312717483cd4f33d3435218e8c8707545
Lucius <lucianoporta@outlook.com>
2020-06-01 23:19:45 -0300

    Le agregue webpack para compilar scss

--- memet/memet/resources/views/components/header.blade.php
+++ memet/memet/resources/views/components/header.blade.php
@@ -1,28 +1,29 @@
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-darker">
     <a class="navbar-brand" href="/">Memet</a>
+
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="/">Link</a>
+                <a class="nav-link" href="{{ route('index') }}">Link</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/subir">Subir meme</a>
+                <a class="nav-link" href="{{ route('altaMeme') }}">Subir meme</a>
             </li>
         </ul>
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/login">Login</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="/registro">Registrate!</a>
-            </li>
-        </ul>
-        <form class="form-inline mt-2 mt-md-0">
+        <form class="form-inline mt-2 mt-md-0 ml-auto mr-2">
             <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Buscar">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
         </form>
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('login') }}">Login</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="href="{{ route('altaUser') }}"">Registrate!</a>
+            </li>
+        </ul>
     </div>
 </nav>
--- memet/memet/routes/web.php
+++ memet/memet/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('altaUser');
+    return view('index');
 });
 
 Route::post('/crearCuenta','UserController@store')->name('store');
